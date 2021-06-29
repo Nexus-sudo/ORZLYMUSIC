e9d1ecbe9275557260e4eea399e19a96cca3ee27
Nexus-sudo <82971853+Nexus-sudo@users.noreply.github.com>
2021-06-29 16:39:04 +0100

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODU5MTA0NjU5OTQ2NzMzNTc5.YNn1kA.nTg6FdFYNtJ1PkxJNrIxZnuPrlI',
+        prefix: 'e!',
+        activity: 'Ernest™ | V.1.0 | Shard 3',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
